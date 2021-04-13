3e1c68942e94e65b0f280409cc91ca1fb9535d2b
jeyao <Jeyych690104@>
2021-04-13 21:05:58 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1e777b6092d9073bc08e.js"
+    "url": "webpack-runtime-018ef8da604ca4dc5b7c.js"
   },
   {
     "url": "framework-d192e43e3504d66786ef.js"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "f0e45107-dddf2e792ec5c6c002bc.js"
   },
   {
-    "url": "app-738596e59d3788dfb1e3.js"
+    "url": "app-e3f1d64e16b704885a7f.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "354c6d45ab50450f2f726d07651b368b"
+    "revision": "8e3e73d76036e39207890b4d5b03a331"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-891f28db69e52dc95827.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "a936ddb44ae95a4c6886a21395305ff0"
+    "revision": "b0cd89a1d40f8d406a3a1e99fb0b6b7d"
   },
   {
     "url": "polyfill-9cdf87ac7248f4b5f755.js"
@@ -153,7 +153,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-738596e59d3788dfb1e3.js`))) {
+  if (!resources || !(await caches.match(`/app-e3f1d64e16b704885a7f.js`))) {
     return await fetch(event.request)
   }
 
